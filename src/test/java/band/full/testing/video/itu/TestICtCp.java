09928569bfd118390ad29033fa6cc2eee3c16453
Igor Malinin <igorzep@gmail.com>
2018-02-04 03:22:57 +0200

    tests passing

--- src/test/java/band/full/testing/video/itu/TestICtCp.java
+++ src/test/java/band/full/testing/video/itu/TestICtCp.java
@@ -4,6 +4,7 @@ import static band.full.testing.video.itu.BT2020.PRIMARIES;
 import static band.full.testing.video.itu.ColorRange.NARROW;
 import static band.full.testing.video.itu.ICtCp.ITPtoPQLMS;
 import static band.full.testing.video.smpte.ST2084.PQ;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,7 @@ import org.junit.jupiter.api.Test;
  * @author Igor Malinin
  */
 public class TestICtCp {
-    private static final ICtCp PARAMS = new ICtCp(PQ, PRIMARIES, 12);
+    private static final ICtCp MATRIX = new ICtCp(PQ, PRIMARIES, 12);
 
     @Test
     public void valuesITPtoPQLMS() {
@@ -29,11 +30,11 @@ public class TestICtCp {
 
     @Test
     public void valuesFull() {
-        assertEquals(0, PARAMS.YMIN);
-        assertEquals(4095, PARAMS.YMAX);
-        assertEquals(1, PARAMS.CMIN);
-        assertEquals(4095, PARAMS.CMAX);
-        assertEquals(2048, PARAMS.ACHROMATIC);
+        assertEquals(0, MATRIX.YMIN);
+        assertEquals(4095, MATRIX.YMAX);
+        assertEquals(1, MATRIX.CMIN);
+        assertEquals(4095, MATRIX.CMAX);
+        assertEquals(2048, MATRIX.ACHROMATIC);
     }
 
     @Test
@@ -49,43 +50,67 @@ public class TestICtCp {
 
     @Test
     public void fromLumaCode() {
-        assertEquals(0.0, PARAMS.fromLumaCode(PARAMS.YMIN));
-        assertEquals(1.0, PARAMS.fromLumaCode(PARAMS.YMAX));
+        assertEquals(0.0, MATRIX.fromLumaCode(MATRIX.YMIN));
+        assertEquals(1.0, MATRIX.fromLumaCode(MATRIX.YMAX));
     }
 
     @Test
     public void fromChromaCode() {
-        assertEquals(-0.5, PARAMS.fromChromaCode(PARAMS.CMIN));
-        assertEquals(0.5, PARAMS.fromChromaCode(PARAMS.CMAX));
-        assertEquals(0.0, PARAMS.fromChromaCode(PARAMS.ACHROMATIC));
+        assertEquals(-0.5, MATRIX.fromChromaCode(MATRIX.CMIN));
+        assertEquals(0.5, MATRIX.fromChromaCode(MATRIX.CMAX));
+        assertEquals(0.0, MATRIX.fromChromaCode(MATRIX.ACHROMATIC));
     }
 
     @Test
     public void toLumaCode() {
-        assertEquals(PARAMS.YMIN, PARAMS.toLumaCode(0.0));
-        assertEquals(PARAMS.YMAX, PARAMS.toLumaCode(1.0));
+        assertEquals(MATRIX.YMIN, MATRIX.toLumaCode(0.0));
+        assertEquals(MATRIX.YMAX, MATRIX.toLumaCode(1.0));
     }
 
     @Test
     public void toChromaCode() {
-        assertEquals(PARAMS.CMIN, PARAMS.toChromaCode(-0.5));
-        assertEquals(PARAMS.CMAX, PARAMS.toChromaCode(0.5));
-        assertEquals(PARAMS.ACHROMATIC, PARAMS.toChromaCode(0.0));
+        assertEquals(MATRIX.CMIN, MATRIX.toChromaCode(-0.5));
+        assertEquals(MATRIX.CMAX, MATRIX.toChromaCode(0.5));
+        assertEquals(MATRIX.ACHROMATIC, MATRIX.toChromaCode(0.0));
     }
 
     @Test
     public void black() {
-        // TODO
+        double[] expected = {0.0, 0.0, 0.0};
+        double[] black = {0.0, 0.0, 0.0};
+        assertArrayEquals(expected, MATRIX.toRGB(black, black));
+        assertArrayEquals(expected, MATRIX.toLinearRGB(black, black));
+        assertArrayEquals(expected, MATRIX.fromRGB(black, black));
+        assertArrayEquals(expected, MATRIX.fromLinearRGB(black, black));
     }
 
     @Test
     public void gray() {
-        // TODO
+        double[] tmp = new double[3];
+        double[] rgb = {0.5, 0.5, 0.5};
+        double[] itp = {0.5, 0.0, 0.0};
+
+        assertColorEquals(0.5, 0.5, 0.5, MATRIX.toRGB(itp, tmp));
+        assertColorEquals(0.5, 0.0, 0.0, MATRIX.fromRGB(rgb, tmp));
     }
 
     @Test
     public void white() {
-        // TODO
+        double[] tmp = new double[3];
+        double[] rgb = {1.0, 1.0, 1.0};
+        double[] itp = {1.0, 0.0, 0.0};
+
+        assertColorEquals(1.0, 1.0, 1.0, MATRIX.toRGB(itp, tmp));
+        assertColorEquals(1.0, 1.0, 1.0, MATRIX.toLinearRGB(itp, tmp));
+        assertColorEquals(1.0, 0.0, 0.0, MATRIX.fromRGB(rgb, tmp));
+        assertColorEquals(1.0, 0.0, 0.0, MATRIX.fromLinearRGB(rgb, tmp));
+    }
+
+    private void assertColorEquals(double c0, double c1, double c2,
+            double[] actual) {
+        assertEquals(c0, actual[0], 1e-15);
+        assertEquals(c1, actual[1], 1e-15);
+        assertEquals(c2, actual[2], 1e-15);
     }
 
     @Test
@@ -107,14 +132,29 @@ public class TestICtCp {
     }
 
     private void symmetry(double r, double g, double b) {
+        symmetryE(r, g, b);
+        symmetryO(r, g, b);
+    }
+
+    private void symmetryE(double r, double g, double b) {
         double[] rgb = {r, g, b};
-        double[] itp = new double[3];
 
-        PARAMS.fromRGB(rgb, itp);
-        PARAMS.toRGB(itp, rgb);
+        MATRIX.fromRGB(rgb, rgb);
+        MATRIX.toRGB(rgb, rgb);
 
-        assertEquals(r, rgb[0], 1e-13);
-        assertEquals(g, rgb[1], 1e-13);
-        assertEquals(b, rgb[2], 1e-13);
+        assertEquals(r, rgb[0], 5e-6);
+        assertEquals(g, rgb[1], 5e-6);
+        assertEquals(b, rgb[2], 5e-6);
+    }
+
+    private void symmetryO(double r, double g, double b) {
+        double[] rgb = {r, g, b};
+
+        MATRIX.fromLinearRGB(rgb, rgb);
+        MATRIX.toLinearRGB(rgb, rgb);
+
+        assertEquals(r, rgb[0], 5e-13);
+        assertEquals(g, rgb[1], 5e-13);
+        assertEquals(b, rgb[2], 5e-13);
     }
 }
