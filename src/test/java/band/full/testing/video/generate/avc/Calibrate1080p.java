5cd30231629e384f81842bd029fc72fd42b65350
Igor Malinin <igorzep@gmail.com>
2018-03-24 03:23:39 +0200

    Skip lossless with AVC

--- src/test/java/band/full/testing/video/generate/avc/Calibrate1080p.java
+++ src/test/java/band/full/testing/video/generate/avc/Calibrate1080p.java
@@ -2,6 +2,7 @@ package band.full.testing.video.generate.avc;
 
 import static band.full.testing.video.core.Resolution.STD_1080p;
 import static band.full.testing.video.encoder.EncoderParameters.FULLHD_MAIN8;
+import static band.full.testing.video.executor.GenerateVideo.Type.MAIN;
 import static band.full.testing.video.generator.GeneratorFactory.AVC;
 
 import band.full.testing.video.executor.FxDisplay;
@@ -13,7 +14,7 @@ import band.full.testing.video.generator.CalibrationBase;
  *
  * @author Igor Malinin
  */
-@GenerateVideo
+@GenerateVideo(MAIN)
 public class Calibrate1080p extends CalibrationBase {
     public Calibrate1080p() {
         super(AVC, FULLHD_MAIN8, "FullHD/Calibrate", "1080p");
